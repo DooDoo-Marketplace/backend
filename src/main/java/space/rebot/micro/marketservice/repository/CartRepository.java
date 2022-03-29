f7f8ed3fb3ff255ae1695f860846805da90a5679
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2022-03-29 14:51:47 +0300

    DBEB-8 added work with cart (#14)

--- src/main/java/space/rebot/micro/marketservice/repository/CartRepository.java
+++ src/main/java/space/rebot/micro/marketservice/repository/CartRepository.java
@@ -1,15 +1,33 @@
 package space.rebot.micro.marketservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import space.rebot.micro.marketservice.model.Cart;
 import space.rebot.micro.marketservice.model.Sku;
 import space.rebot.micro.userservice.model.User;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository <Cart, Long> {
-    List<Cart> getCartByUser(User user);
-    List<Cart> getCartBySku(Sku sku);
+
+    @Query(value = "select sku_id from cart c " +
+            "where c.user_id = :userId and c.is_deleted = false", nativeQuery = true)
+    List<Long> getSkuIdsByUserId(@Param("userId") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update cart c set count = :cnt " +
+            "where c.user_id = :userId and c.sku_id = :skuId and c.is_deleted = false", nativeQuery = true)
+    int updateSkuCnt(@Param("userId") Long userId, @Param("skuId") Long skuId, @Param("cnt") int cnt);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update cart c set is_deleted = true " +
+            "where c.user_id = :userId and c.sku_id = :skuId and c.is_deleted = false", nativeQuery = true)
+    int markDelete(@Param("userId") Long userId, @Param("skuId") Long skuId);
 }
