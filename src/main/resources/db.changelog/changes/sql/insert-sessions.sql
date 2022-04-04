8b46ab1ba5e80b2333c6182c81e044b287703452
Gosha <kapgosha@gmail.com>
2022-04-04 19:02:44 +0300

    DBEB-18 переделал в sql, добавил токены

---
+++ src/main/resources/db.changelog/changes/sql/insert-sessions.sql
@@ -0,0 +1,14 @@
+INSERT INTO sessions(user_id, token)
+VALUES ('1001', '12341');
+
+INSERT INTO sessions(user_id, token)
+VALUES ('1002', '12342');
+
+INSERT INTO sessions(user_id, token)
+VALUES ('1003', '12343');
+
+INSERT INTO sessions(user_id, token)
+VALUES ('1004', '12344');
+
+INSERT INTO sessions(user_id, token)
+VALUES ('1005', '12345');
---
+++ src/main/resources/db.changelog/changes/sql/insert-users-roles.sql
@@ -0,0 +1,14 @@
+INSERT INTO users_roles(user_id, roles_id)
+VALUES ('1001', '1');
+
+INSERT INTO users_roles(user_id, roles_id)
+VALUES ('1002', '1');
+
+INSERT INTO users_roles(user_id, roles_id)
+VALUES ('1003', '1');
+
+INSERT INTO users_roles(user_id, roles_id)
+VALUES ('1004', '1');
+
+INSERT INTO users_roles(user_id, roles_id)
+VALUES ('1005', '1');
