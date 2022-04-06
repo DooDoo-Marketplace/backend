2b4ab3b3a5a4ffea8e870baa6ab3cff57c99daba
Nikolay-Gonichenko <74869282+Nikolay-Gonichenko@users.noreply.github.com>
2022-04-06 13:52:19 +0300

    Dbeb 16 (#24)
    * DBEB-16 added scheduler service, make job for deleting carts where is_deleted=true from db
    
    * DBEB-16 fixed scheduler, make opportunity for creating jobs in xml
    
    * DBEB-16 fixed, tested, deleted sql scripts and changed configuration
    
    * DBEB-16 bug fixed

--- src/main/java/space/rebot/micro/marketservice/repository/CartRepository.java
+++ src/main/java/space/rebot/micro/marketservice/repository/CartRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import space.rebot.micro.marketservice.model.Cart;
-import space.rebot.micro.marketservice.model.Sku;
-import space.rebot.micro.userservice.model.User;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -35,8 +33,14 @@ public interface CartRepository extends JpaRepository <Cart, Long> {
     int updateSkuCnt(@Param("userId") Long userId, @Param("skuId") Long skuId, @Param("cnt") int cnt,
                              @Param("cartStatusId") int cartStatusId, @Param("isRetail") boolean isRetail);
 
+    @Query(value = "select c.id from cart c where c.cart_status_id = :cartStatus limit :count", nativeQuery = true)
+    List<Long> getCartByDeletedStatus(@Param("cartStatus") int status, @Param("count") int count);
+
     @Modifying
     @Transactional
+    @Query(value = "delete from cart c where c.id in :ids", nativeQuery = true)
+    int deleteCartByIdList(@Param("ids") List<Long> ids);
+
     @Query(value = "update cart c set cart_status_id = :exposedStatusId " +
             "where c.sku_id = :skuId and c.user_id = :userId " +
             "and c.cart_status_id = :updatedStatusId and is_retail = :isRetail", nativeQuery = true)
