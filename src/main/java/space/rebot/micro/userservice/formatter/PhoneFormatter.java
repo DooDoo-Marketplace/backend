77c9aba8f9eefa71dd2a0dfbaa9698ab83db1eb3
Gosha <kapgosha@gmail.com>
2022-04-10 19:35:26 +0300

    DBEB-19 В PhoneFormatter теперь кидается InvalidPhoneException

--- src/main/java/space/rebot/micro/userservice/formatter/PhoneFormatter.java
+++ src/main/java/space/rebot/micro/userservice/formatter/PhoneFormatter.java
@@ -1,7 +1,14 @@
 package space.rebot.micro.userservice.formatter;
 
+import space.rebot.micro.userservice.exception.InvalidPhoneException;
+import space.rebot.micro.userservice.validator.PhoneValidator;
+
 public class PhoneFormatter {
-    public String format(String phone) {
+    public String format(String phone) throws Exception {
+        PhoneValidator phoneValidator = new PhoneValidator();
+        if (phoneValidator.validate(phone, false)) {
+            throw new InvalidPhoneException();
+        }
         if (phone.charAt(0) == '8' || phone.charAt(0) == '7') {
             phone = "+7" + phone.substring(1);
         } else if (phone.charAt(0) == '+' && phone.charAt(1) == '8') {
